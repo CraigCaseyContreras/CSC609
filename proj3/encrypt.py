7cf102eb969a766bf8f774a4b23aff9a4d2d17ca
CraigCaseyContreras <craigcc96@outlook.com>
2019-10-07 19:09:32 -0400

    semi final encrypt

--- proj3/encrypt.py
+++ proj3/encrypt.py
@@ -8,9 +8,10 @@ from Bozhu_AES import AES
 #
 # encrypt.py
 #
-# author:
-# date:
-# last update:
+# author: Victoria Roddy
+# date: 6 October 2019
+# last update: 6 October 2019
+# I WORKED WITH: Craig and Lucas
 #
 # template by: bjr sep 2019
 # template update: 24 sep 2019
@@ -19,118 +20,300 @@ from Bozhu_AES import AES
 args_g = 0  # args are global
 
 BLOCK_SIZE = 16  # the AES block size
-KEY_SIZE = 16    # the AES key size
+KEY_SIZE = 16  # the AES key size
+
+
+def pkcs_padding(plaintext):
+    # CODE TAKEN FROM __init__.py file
+    padding_len = 16 - (len(plaintext) % 16)
+    padding = bytes([padding_len] * padding_len)
+    return plaintext + padding
+
+
+def remove_pkcs_padding(plaintext):
+    # CODE TAKEN FROM __init__.py file
+    padding_len = plaintext[-1]
+    assert padding_len > 0
+    message, padding = plaintext[:-padding_len], plaintext[-padding_len:]
+    assert all(p == padding_len for p in padding)
+    return message
+
+
+def zero_padding(plaintext):
+    padding_length = 16 - (len(plaintext) % 16)
+    padding = bytes([0] * padding_length)
+    return plaintext + padding
+
+
+def remove_zero_padding(ciphertext):
+    cipher_strip = ciphertext.strip(b'\x00')
+    return cipher_strip
+
+
+def iso_padding(plaintext):
+    padding_length = 16 - (len(plaintext) % 16)
+    padding = b'\x80' + bytes([0] * (padding_length-1))
+    return plaintext + padding
+
+
+def remove_iso_padding(plaintext):
+    plain_strip = plaintext.strip(b'\x00')
+    plain_strip2 = plain_strip.strip(b'\x80')
+    return plain_strip2
+
+def crypt_ecb(aes, intext):
+ 
+    if args_g.decrypt:
+
+        decryption_blocks = []
+        block = [intext[i:i+16] for i in range(0, len(intext), 16)]
+        for i in range(0, len(block)):
+            ciph_block = block[i]
+            decryption_blocks.append(aes.decrypt_block(ciph_block))
+            result = b''.join(decryption_blocks)
+        if args_g.padding == "pkcs":
+            output = remove_pkcs_padding(result)
+        elif args_g.padding == "zero":
+            output = remove_zero_padding(result)
+        elif args_g.padding == "iso":
+            output = remove_iso_padding(result)
+        else:
+            output = remove_pkcs_padding(result)
+
+    else:
+
+        # do encryption
+        blobs = []
+        if args_g.padding == "pkcs":
+            padded_messg = pkcs_padding(intext)
+        elif args_g.padding == "zero":
+            padded_messg = zero_padding(intext)
+        elif args_g.padding == "iso":
+            padded_messg = iso_padding(intext)
+        else:
+            padded_messg = pkcs_padding(intext)
+        for i in range(0, len(padded_messg), 16):
+            ptBlocks = padded_messg[i:i + 16]
+            res = aes.encrypt_block(ptBlocks)
+            blobs.append(res)
+        output = b''.join(blobs)
+        
+    return output
+
+def crypt_cntr(aes, intext):
+    IV = os.urandom(16)
+
+    if args_g.decrypt:
+
+        decryption_blocks = []
+        block = [intext[i:i + 16] for i in range(0, len(intext), 16)]
+        IV = block[0]
+        block.pop(0)
+        iv_block = IV
+        for i in range(0, len(block)):
+            ciph_block = block[i]
+            jul = aes.encrypt_block(iv_block)
+            decryption_blocks.append(aes.xor_bytes(jul, ciph_block))
+
+            # INCREMENTS COUNTER BY +1
+            iv_int = int.from_bytes(iv_block, "big")
+            iv_int_inc = iv_int + 1
+            iv_block = iv_int_inc.to_bytes(16, byteorder="big")
+
+        if args_g.padding == "pkcs":
+            output = remove_pkcs_padding(b''.join(decryption_blocks))
+        elif args_g.padding == "zero":
+            output = remove_zero_padding(b''.join(decryption_blocks))
+        elif args_g.padding == "iso":
+            output = remove_iso_padding(b''.join(decryption_blocks))
+        else:
+            output = remove_pkcs_padding(b''.join(decryption_blocks))
+    
+    else:
+
+        if args_g.padding == "pkcs":
+            padded_messg = pkcs_padding(intext)
+        elif args_g.padding == "zero":
+            padded_messg = zero_padding(intext)
+        elif args_g.padding == "iso":
+            padded_messg = iso_padding(intext)
+        else:
+            padded_messg = pkcs_padding(intext)
+        blobs = []
+        iv_block = IV
+        for i in range(0, len(padded_messg), 16):
+            ptBlocks = padded_messg[i:i + 16]
+            encrypt_iv = aes.encrypt_block(iv_block)
+            block = aes.xor_bytes(encrypt_iv, ptBlocks)
+            blobs.append(block)
+
+            # INCREMENTS COUNTER BY +1
+            iv_int = int.from_bytes(iv_block, "big")
+            iv_int_inc = iv_int + 1
+            iv_block = iv_int_inc.to_bytes(16, byteorder="big")
+
+        blobs.insert(0, IV)
+        output = b''.join(blobs)
+
+    return output
 
 def crypt_cbc(aes, intext):
+    IV = os.urandom(16)
 
-	IV = b'\01' * 16
+    if args_g.decrypt:
 
-	if args_g.decrypt:
-		#This intext is then a ciphertext
-		blokess = []
-		prevDecrypt = IV
+        decryption_blocks = []
+        block = [intext[i:i + 16] for i in range(0, len(intext), 16)]
+        iv_block = block[0]
+        block.pop(0)
+        # Splits in 16-byte parts.
+        for i in range(0, len(block)):
+            ciph_block = block[i]
+            decryption_blocks.append(aes.xor_bytes(iv_block, aes.decrypt_block(ciph_block)))
+            iv_block = ciph_block
+            result = b''.join(decryption_blocks)
+        if args_g.padding == "pkcs":
+            output = remove_pkcs_padding(result)
+        elif args_g.padding == "zero":
+            output = remove_zero_padding(result)
+        elif args_g.padding == "iso":
+            output = remove_iso_padding(result)
+        else:
+            output = remove_pkcs_padding(result)
+        #return output
 
-		#Split in 16 byte parts
-		#The following need to be indented
-		for i in range(0, len(intext), 16):
-			ciph_block = intext[i:i+16]
-			blokess.append(aes.xor_bytes(prevDecrypt, aes.decrypt_block(ciph_block)))
-			prevDecrypt = ciph_block
-		outext = aes.unpad(b''.join(blokess))
+    else:
 
-	else:
-		#This intext is a plaintext
-		#MUST ENCODE OR WONT LET YOU PAD!!!
+        if args_g.padding == "pkcs":
+            padded_messg = pkcs_padding(intext)
+        elif args_g.padding == "zero":
+            padded_messg = zero_padding(intext)
+        elif args_g.padding == "iso":
+            padded_messg = iso_padding(intext)
+        else:
+            padded_messg = pkcs_padding(intext)
+        blobs = []
+        xor_block = IV
+        # Splits in 16-byte parts.
+        for i in range(0, len(padded_messg), 16):
+            ptBlocks = padded_messg[i:i + 16]
+            # CBC mode encrypt: encrypt(plaintext_block XOR previous)
+            blockCT = aes.encrypt_block(aes.xor_bytes(ptBlocks, xor_block))
+            blobs.append(blockCT)
+            last_ciphertext = blockCT
+        blobs.insert(0, IV)
+        output = b''.join(blobs)
 
-		message = (intext.encode('utf-8'))
-		message = bytearray(message)
+    return output
 
-		#pad the message
-		paddedmessage = aes.pad(message)
-		#The code does the encrypt WITH the xor at the same time
+def crypt_ofb(aes, intext):
+    iv = os.urandom(16)
 
-		blokes = []
-		prev = IV
+    if args_g.decrypt:
 
-		#Now we have to split into 16-byte parts
-		for l in range(0, len(paddedmessage), 16):
-			paddedBlock = paddedmessage[l:l+16]
-			#CBC mode so XOR the block with before
-			bloke = aes.encrypt_block(aes.xor_bytes(paddedBlock, prev))
-			blokes.append(bloke)
-			prev = bloke
-		#print(blokes[0], len(blokes[0])) #Blocks are working!!!
-		outext = b''.join(blokes)
-	return outext
+        decryption_blocks = []
+        # splits cipher test into blocks of size 16
+        block = [intext[i:i + 16] for i in range(0, len(intext), 16)]
+        iv_block = block[0]
+        block.pop(0)
+        # Splits in 16-byte parts.
+        for i in range(0, len(block)):
+            ciphertext_block = block[i]
+            # CBC mode decrypt: previous XOR decrypt(ciphertext)
+            # decryption_blocks.append(aes.xor_bytes(iv_block, aes.decrypt_block(iv_block)))
+            encrypted_iv = aes.encrypt_block(iv_block)
+            iv_block = encrypted_iv
+            decryption_blocks.append(aes.xor_bytes(encrypted_iv, ciphertext_block))
+            result = b''.join(decryption_blocks)
+        if args_g.padding == "pkcs":
+            output = remove_pkcs_padding(b''.join(decryption_blocks))
+        elif args_g.padding == "zero":
+            output = remove_zero_padding(b''.join(decryption_blocks))
+        elif args_g.padding == "iso":
+            output = remove_iso_padding(b''.join(decryption_blocks))
+        else:
+            output = remove_pkcs_padding(b''.join(decryption_blocks))
+       
+    else:
 
-def crypt_ecb(aes,intext):
-	"""
-	encrypts or decrypts text using AES object, in ECB mode
-	"""
+        if args_g.padding == "pkcs":
+            padded_messg = pkcs_padding(intext)
+        elif args_g.padding == "zero":
+            padded_messg = zero_padding(intext)
+        elif args_g.padding == "iso":
+            padded_messg = iso_padding(intext)
+        else:
+            padded_messg = pkcs_padding(intext)
+        blobs = []
+        xor_block = iv
+        
+        for i in range(0, len(padded_messg), 16):
+            ptBlocks = padded_messg[i:i + 16]
+            # CBC mode encrypt: encrypt(plaintext_block XOR previous)
+            jul = aes.encrypt_block(xor_block)
+            xor_block = jul
+            block = aes.xor_bytes(xor_block, ptBlocks)
+            blobs.append(block)
+        blobs.insert(0, iv)
+        output = b''.join(blobs)
 
-	# this is just a little test code. It only encrypts exactly one block,
-	# using zero padding
-	intext = (intext + bytes(BLOCK_SIZE))[0:BLOCK_SIZE]
-	print(intext, "HAHAHAHA")
-	assert(len(intext)==BLOCK_SIZE)
-
-	if args_g.decrypt:
-	# do decryption
-		outtext = aes.decrypt_block(intext)
-
-	else:
-	#do encryption
-		outtext = aes.encrypt_block(intext)
-
-	return outtext
+    return output
 
 
+def crypt_func(aes, intext, mode):
+    output = b''
+    if mode == "ecb":
+        output = crypt_ecb(aes, intext)
+    elif mode == "cbc":
+        output = crypt_cbc(aes, intext)
+    elif mode == "ofb":
+        output = crypt_ofb(aes, intext)
+    elif mode == "cntr":
+        output = crypt_cntr(aes, intext)
+    return output
 
 
 def parse_args():
-	parser = argparse.ArgumentParser(description="Encrypt/decrypt stdin. ")
-	parser.add_argument("key", help="encipherment key")
-	parser.add_argument("-d", "--decrypt", action="store_true", help="decrypt, instead of encrypting")
-	parser.add_argument("-m", "--mode", help="mode either cntr (default), cbc, ofb, or ecb")
-	parser.add_argument("-n", "--nonce", help="the initial vector, as ascii. omit for a random nonce (recommended)")
-	parser.add_argument("-p", "--padding", help="padding either pkcs (default), iso, or zero")
-	parser.add_argument("-v", "--verbose", action="store_true", help="verbose")
-	return parser.parse_args()
+    parser = argparse.ArgumentParser(description="Encrypt/decrypt stdin. ")
+    parser.add_argument("key", help="encipherment key")
+    parser.add_argument("-d", "--decrypt", action="store_true", help="decrypt, instead of encrypting")
+    parser.add_argument("-m", "--mode", help="mode either cntr (default), cbc, ofb, or ecb")
+    parser.add_argument("-n", "--nonce", help="the initial vector, as ascii. omit for a random nonce (recommended)")
+    parser.add_argument("-p", "--padding", help="padding either pkcs (default), iso, or zero")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose")
+    return parser.parse_args()
 
-modes = ["cntr","cbc","ofb","cntr"]
-pads = ["pkcs","iso","zero"]
+
+modes = ["ecb", "cbc", "ofb", "cntr"]
+pads = ["pkcs", "iso", "zero"]
+
 
 def main(argv):
+    global args_g
+    args_g = parse_args()
 
-	global args_g
-	args_g = parse_args()
+    if args_g.mode not in modes:
+        args_g.mode = modes[0]
+    if args_g.padding not in pads:
+        args_g.padding = pads[0]
 
-	if args_g.mode not in modes:
-		args_g.mode = modes[0]
-	if args_g.padding not in pads:
-		args_g.padding = pads[0]
+    ## check args
+    if args_g.verbose:
+        print("command line arguments-")
+        print("\t-d:", args_g.decrypt)
+        print("\t-m:", args_g.mode)
+        print("\t-n:", args_g.nonce)
+        print("\t-p:", args_g.padding)
+        print("\t-v:", args_g.verbose)
+        print("\tkey:", args_g.key)
 
-	## check args
-	if args_g.verbose:
-		print("command line arguments-")
-		print("\t-d:", args_g.decrypt)
-		print("\t-m:", args_g.mode)
-		print("\t-n:", args_g.nonce)
-		print("\t-p:", args_g.padding)
-		print("\t-v:", args_g.verbose)
-		print("\tkey:", args_g.key)
-
-	aes = AES(bytes(KEY_SIZE))
-
-	try:
-		bintext = sys.stdin.buffer.read()
-		#print(bintext, "+++bintext1")
-		bintext = crypt_ecb(aes,bintext)
-		#print(bintext)
-		#print(bintext, "+++bintext2 of length: ", len(bintext))
-		sys.stdout.buffer.write(bintext)
-	except Exception:
-		pass
+    aes = AES(bytes(KEY_SIZE))
+    try:
+        bintext = sys.stdin.buffer.read()
+        bintext = crypt_func(aes, bintext, args_g.mode)
+        sys.stdout.buffer.write(bintext)
+    except Exception:
+        pass
 
 
 main(sys.argv)
--- proj3/testencrypt.py
+++ proj3/testencrypt.py
@@ -3,6 +3,7 @@ import string
 import sys
 import os
 import argparse
+import testClasses as pyaes
 from Bozhu_AES import AES
 
 #
@@ -33,107 +34,45 @@ KEY_SIZE = 16    # the AES key size
 def _concat_list(a,b):
 	return a + bytes(b)
 
-def crypt_ofb(aes, intext):
+#PADDINGS!!!
 
-	return
+#This is the ISO padding
+def iso_padding(plaintext):
+    pad_length = 16-(len(plaintext)%16)
+    pad = b'\x80' + bytes([0] * (pad_length -1))
+    return plaintext + pad
 
+def remove_iso_padding(plaintext):
+    stripper = plaintext.strip(b'\x00')
+    second_stripper = stripper.strip(b'\x80')
+    return second_stripper
+
+#This is the 0 padding
+def zero_pading(plaintext):
+    pad_length = 16-(len(plaintext)%16)
+    pad = b'\x00' + bytes([0] * (pad_length -1))
+    return plaintext + pad
+
+def remove_zero_padding(plaintext):
+    stripper = plaintext.strip(b'\x00')
+    return stripper
+
+#This is the PkCS #7 padding
+def pkcs_padding(aes, plaintext):
+    pad_message = aes.pad(plaintext)
+    return pad_message
+
+def remove_pkcs(aes, plaintext):
+    unpadded_message = aes.unpad(plaintext)
+    return unpadded_message
 #-------------------------------------------
 
-def crypt_cbc(aes, intext, mode):
-	#our intext right now is "hello world" so really all we have is "hello world" as the paramter
-	#intext can also be a ciphertext so can be either one
-	#if args.v.encrypt, intext is a plaintext,
-	#if args.v.decrypt, intext is a ciphertext
-	# Get the IV
-	IV = b'\01' * 16
-	#print(IV, "This is the IV of length: ", len(IV))
-
-	if mode == "decrypt":
-	#	print("Need to decrypt")
-		#This intext is then a ciphertext
-		blokess = []
-		prevDecrypt = IV
-
-		#Split in 16 byte parts
-		#The following need to be indented
-		for i in range(0, len(intext), 16):
-			ciph_block = intext[i:i+16]
-			blokess.append(aes.xor_bytes(prevDecrypt, aes.decrypt_block(ciph_block)))
-			prevDecrypt = ciph_block
-		outext = aes.unpad(b''.join(blokess))
-		#print(blokess) # blocks are working
-		print (outext, "Result from the CBC - decrypt")
-		# return outext
-
-	else:
-		#This intext is a plaintext
-		#intextt = (intext.encode('utf-8') + bytes(BLOCK_SIZE))[0:BLOCK_SIZE]
-		#zeroCipher = aes.encrypt_block(intextt)
-		#print(zeroCipher, "Result of encrypting using 0 padding")
-
-		#print(intextt, "intext with 0 padding")
-		#MUST ENCODE OR WONT LET YOU PAD!!!
-		#print(intext, "This is the intext of length: ", len(intext))
-		message = intext.encode('utf-8')
-		message = bytearray(message)
-
-		#print(message, "This is the intext encoded of length: ", len(message))
-		#pad the message
-		paddedmessage = aes.pad(message)
-		#print(paddedmessage, "This is the padded message of length: ", len(paddedmessage))
-		#print(intextt, "This is the 0 padded message of length: ", len(intextt))
-		#Now we can encrypt and do the xor? Or do the xor first?
-		#The code does the encrypt WITH the xor at the same time
-
-		blokes = []
-		prev = IV
-
-
-		#Now we have to split into 16-byte parts
-		for l in range(0, len(paddedmessage), 16):
-			paddedBlock = paddedmessage[l:l+16]
-			#CBC mode so XOR the block with before
-			bloke = aes.encrypt_block(aes.xor_bytes(paddedBlock, prev))
-			blokes.append(bloke)
-			prev = bloke
-		#print(blokes[0], len(blokes[0])) #Blocks are working!!!
-		outext = b''.join(blokes)
-		print(outext, "Result from CBC - encrypt")
-	return outext
+def crypt_cbc(aes, intext, mode): #Have to fix the professors!!!
+    if mode == "decrypt":
+        return "hellow"
+    else:
+        return "cool"
 	#----------------------------------------------------------------
-
-# def crypt_cfb(aes, intext, mode):
-#
-# 	IV = b'\01' * 16
-#
-# 	if mode == "decrypt":
-# 		shift_done = IV
-# 		decrypted = []
-# 		for j in range(0, len(intext), 16):
-# 			cipher_segment = intext[j:j+16]
-# 			xor_segment = aes.encrypt_block(shift_done)[:len(cipher_segment)]
-# 			pt_segment = aes.xor_bytes(cipher_segment, xor_segment)
-# 			shift_done = _concat_list(shift_done[len(cipher_segment):], cipher_segment)
-# 			decrypted.append(pt_segment)
-# 		print(b''.join(decrypted), "||" "Result from the CFB - decrypt")
-# 		return (b''.join(decrypted))
-#
-# 	else:
-# 		message = intext.encode('utf-8')
-# 		shift_done = IV
-# 		# pad the message
-# 		#paddedmessage = aes.pad(message)
-# 		encrypted = []
-# 		for i in range(0, len(message), 16):
-# 			pt_segment = message[i: i + 16]
-# 			xor_segment = aes.encrypt_block(shift_done)[:len(pt_segment)]
-# 			cipher_segment = aes.xor_bytes(pt_segment, xor_segment)
-#
-# 			shift_done = _concat_list(shift_done[len(cipher_segment):], cipher_segment)
-# 			encrypted.append(cipher_segment)
-# 		print(b''.join(encrypted), "||" "Result from the CFB - encrypt")
-# 		return (b''.join(encrypted))
-
 
 #---------------------------------------------------
 
@@ -197,13 +136,26 @@ def crypt_cntr(aes, intext, mode):
 def main():
 
 	aes = AES(bytes(KEY_SIZE))
-	message = "My name is Craig Contreras. Attack can commence."
+	message = "My name is Craig Contreras."
 	decrypt = "decrypt"
 	encrypt = "encrypt"
 	ciphertextCBC = crypt_cbc(aes, message, encrypt)
-	messageCBC = crypt_cbc(aes, ciphertextCBC, decrypt)
+	#messageCBC = crypt_cbc(aes, ciphertextCBC, decrypt)
 	ciphertextCNTR = crypt_cntr(aes, message, encrypt)
 	messageCNTR = crypt_cntr(aes, ciphertextCNTR, decrypt)
+	iso_mess = iso_padding(b'Hello World')
+	print(iso_mess, len(iso_mess))
+	deiso = remove_iso_padding(iso_mess)
+	print(deiso)
+	zero_mess = zero_pading(b'hello ghjghjgjgjhghjgjjhgkjhworld')
+	print(zero_mess, len(zero_mess))
+	dezero = remove_zero_padding(zero_mess)
+	print(dezero)
+	pkmess = pkcs_padding(aes, b'hello wjhgjgorld')
+	print(pkmess, len(pkmess))
+	#test_ofb(aes, message)
+	# messageOFB = crypt_ofb(aes, message, encrypt)
+	# ciphertextOFB = crypt_ofb(aes, messageOFB, decrypt)
 
 if __name__== '__main__':
 	main()
